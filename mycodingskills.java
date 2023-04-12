d8b91288e7cc11eaaea4dcc1b9c9d455f2f1d9ba
jrvent <jventura@appdev.yearup.org>
2023-04-12 09:43:58 -0400

    New Java 2

---
+++ mycodingskills.java
@@ -0,0 +1,29 @@
+public static void main(String[]args){
+        system.out.println("Java")
+        }
+
+
+public static void main(String[]args){
+        system.out.println("HTML")
+
+
+        }
+
+public static void main(String[]args){
+        System.out.println("C++")
+        }
+
+
+public static void main(St(string[]args){
+        system.out.println("Css")
+        }
+
+
+public static void main(String[]args){
+        system.out.println("Python")
+        }
+
+
+public static void main(String[]args){
+        system.out.println("Ruby")
+        }
